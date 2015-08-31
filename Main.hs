0a270930b0e9e6b53d0096163458724a58d01acc
Adam Wick <awick@galois.com>
2015-08-31 16:27:18 -0700

    Base flags and main support for the HaLVM. Works for a little while, then crashes. :(

--- Main.hs
+++ Main.hs
@@ -1,40 +1,40 @@
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE CPP #-}
 import Control.Concurrent(forkIO,threadDelay)
 import Control.Exception
 import Control.Monad
-import Crypto.Number.Basic
-import Crypto.PubKey.RSA
-import Data.ASN1.BinaryEncoding
-import Data.ASN1.Encoding
-import Data.ASN1.Types
-import Data.ByteArray.Encoding
-import qualified Data.ByteString as BS
-import qualified Data.ByteString.Char8 as BSC
-import Data.ByteString.Base64
 import Data.ByteString.Char8(pack)
 import Data.Hourglass
 import Data.Hourglass.Now
 import Data.List
 import Data.Word
-import Data.X509
 import Network.TLS
-import System.IO
 import Tor.DataFormat.TorAddress
 import Tor.Circuit
 import Tor.DataFormat.TorCell
 import Tor.Link
 import Tor.NetworkStack
-import Tor.NetworkStack.System
 import Tor.Options
 import Tor.RouterDesc
 import Tor.State
-import Debug.Trace
+
+#ifdef HaLVM_HOST_OS
+import Hans.Device.Xen
+import Hans.DhcpClient
+import Hans.NetworkStack hiding (listen, accept)
+import Hypervisor.Console
+import Hypervisor.XenStore
+import Tor.NetworkStack.Hans
+import XenDevice.NIC
+#else
+import Tor.NetworkStack.System
+#endif
 
 main :: IO ()
 main = runDefaultMain $ \ flags ->
-  do logger        <- generateLogger flags
+  do (ns, logger)  <- initializeSystem flags
      let onionPort =  getOnionPort flags
-     torState      <- initializeTorState systemNetworkStack logger flags
+     torState      <- initializeTorState ns logger flags
      startTorServerPort torState onionPort
      buildCircularCircuit torState
      forever (threadDelay 100000000)
@@ -48,13 +48,17 @@ buildCircularCircuit torState = catch tryCircular notPublic
        logMsg torState "I hope this is an output-only, non-relay node."
   --
   tryCircular =
-    do initRouter <- getRouter torState []
+    do logMsg torState "Getting initial router.\n"
+       initRouter <- getRouter torState []
+       logMsg torState "Creating circuit.\n"
        circ       <- throwLeft =<< createCircuit torState initRouter
+       logMsg torState "Getting local address.\n"
        myAddrs    <- getLocalAddresses torState
        logMsg torState ("My publicly-routable IP address(es): " ++
                         intercalate "," (map show myAddrs))
+       logMsg torState "Getting intermediate router.\n"
        midRouter  <- getRouter torState [NotRouter initRouter]
-       putStrLn ("Extending router to " ++ routerIPv4Address midRouter)
+       logMsg torState ("Extending router to " ++ routerIPv4Address midRouter)
        ()         <- throwLeft =<< extendCircuit torState circ midRouter
        lastRouter <- getRouter torState [NotRouter initRouter,
                                          NotRouter midRouter,
@@ -63,17 +67,17 @@ buildCircularCircuit torState = catch tryCircular notPublic
                                          NotTorAddr (IP4 "162.248.160.151"),
                                          NotTorAddr (IP4 "109.235.50.163"),
                                          ExitNodeAllowing (IP4 "66.193.37.213") 80]
-       putStrLn ("Extending router to " ++ routerIPv4Address lastRouter)
+       logMsg torState ("Extending router to " ++ routerIPv4Address lastRouter)
        ()         <- throwLeft =<< extendCircuit torState circ lastRouter
        nms        <- resolveName circ "galois.com"
-       putStrLn ("Resolved galois.com to " ++ show nms)
-       putStrLn ("Exit rules: " ++ show (routerExitRules lastRouter))
+       logMsg torState ("Resolved galois.com to " ++ show nms)
+       logMsg torState ("Exit rules: " ++ show (routerExitRules lastRouter))
        con        <- connectToHost circ (Hostname "uhsure.com") 80
-       putStrLn ("Built connection!")
+       logMsg torState ("Built connection!")
        torWrite con (buildGet "http://uhsure.com/")
-       putStrLn ("Wrote GET")
+       logMsg torState ("Wrote GET")
        resp <- torRead con 300
-       putStrLn ("Got response: " ++ show resp)
+       logMsg torState ("Got response: " ++ show resp)
        destroyCircuit circ NoReason
   --
   buildGet str = result
@@ -96,15 +100,49 @@ startTorServerPort torState onionPort =
 
 -- -----------------------------------------------------------------------------
 
-generateLogger :: [Flag] -> IO (String -> IO ())
-generateLogger []                 = return (makeLogger stdout)
-generateLogger ((OutputLog fp):_) = makeLogger `fmap` openFile fp AppendMode
-generateLogger (_:rest)           = generateLogger rest
+initializeSystem :: [Flag] ->
+                    IO (TorNetworkStack Socket Socket, String -> IO ())
+#ifdef HaLVM_HOST_OS
+initializeSystem _ =
+  do con    <- initXenConsole
+     xs     <- initXenStore
+     ns     <- newNetworkStack
+     macstr <- findNIC xs
+     putStrLn ("Using NIC with MAC address " ++ macstr)
+     nic    <- openNIC xs macstr
+     putStrLn ("Started the NIC.")
+     let mac = read macstr
+     addDevice ns mac (xenSend nic) (xenReceiveLoop nic)
+     putStrLn ("Added the device.")
+     deviceUp ns mac
+     putStrLn ("Started the device.")
+     ipaddr <- dhcpDiscover ns mac
+     putStrLn ("Node has IP address " ++ show ipaddr)
+     return (hansNetworkStack ns, makeLogger (\ x -> writeConsole con (x ++ "\n")))
+ where
+  findNIC xs =
+    do nics <- listNICs xs
+       case nics of
+         []    -> threadDelay 1000000 >> findNIC xs
+         (x:_) -> return x
+#else
+initializeSystem flags =
+  do logger <- generateLogger flags
+     return (systemNetworkStack, logger)
 
-makeLogger :: Handle -> String -> IO ()
-makeLogger h msg =
+generateLogger :: [Flag] -> IO (String -> IO ())
+generateLogger []                 = return (makeLogger (hPutStrLn stdout))
+generateLogger ((OutputLog fp):_) = do h <- openFile fp AppendMode
+                                       return (makeLogger (hPutStrLn h))
+generateLogger (_:rest)           = generateLogger rest
+#endif
+
+-- -----------------------------------------------------------------------------
+
+makeLogger :: (String -> IO ()) -> String -> IO ()
+makeLogger out msg =
   do now <- getCurrentTime
-     hPutStrLn h (timePrint timeFormat now ++ msg)
+     out (timePrint timeFormat now ++ msg)
  where
   timeFormat = [Format_Text '[', Format_Year4, Format_Text '-', Format_Month2,
                 Format_Text '-', Format_Day2, Format_Text ' ', Format_Hour,
